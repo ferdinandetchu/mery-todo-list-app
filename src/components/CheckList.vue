254699b59cad420cb38a7d24d4ed0d67796c6182
ferdinandetchu <47149175+ferdinandetchu@users.noreply.github.com>
2022-07-20 15:49:23 +0100

    mery todo fully build ui with computed

---
+++ src/components/CheckList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-list-item v-for="(todo, index) in todos" :key="index">
+      <v-list-item-action>
+        <v-checkbox
+          @click="checkedTodo(todo)"
+          input-value
+          color="primary"
+        ></v-checkbox>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>{{ todo.title }}</v-list-item-title>
+        <!-- <v-list-item-subtitle>Descrition</v-list-item-subtitle> -->
+      </v-list-item-content>
+      <v-list-item-icon>
+        <v-btn icon>
+          <v-icon> mdi-message-outline </v-icon>
+        </v-btn>
+      </v-list-item-icon>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckList",
+  props: {
+    todos: Array,
+  },
+  // data: () => ({}),
+  // emits: ["check"],
+  methods: {
+    checkedTodo(todo) {
+      this.$emit("check", todo);
+      // console.log(todo);
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,117 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container style="height: 100vh">
+    <v-row justify="center" align="center" class="fill-height">
+      <v-card min-width="350" max-width="475" class="mx-auto">
+        <!-- <h1>Todo List</h1> -->
+        <v-toolbar color="teal" dark>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Todo List</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn v-if="!isAdd" fab small @click="isAdd = true">
+            <v-icon> mdi-plus </v-icon>
+          </v-btn>
+        </v-toolbar>
+
+        <v-text-field
+          label="Input Todo"
+          placeholder="What needs to be done?"
+          solo
+          append-icon="mdi-plus"
+          ref="title"
+          v-model="newTodo"
+          v-if="isAdd"
+          :rules="[() => !!newTodo || 'This field is required']"
+          @click:append="addTodo()"
+        ></v-text-field>
+
+        <v-list subheader>
+          <v-subheader class="red--text">Things Todo</v-subheader>
+          <CheckList
+            @check="ChangeState($event)"
+            v-if="thingsTodo.length > 0"
+            :todos="thingsTodo"
+          />
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list subheader>
+          <v-subheader class="amber--text">Doing</v-subheader>
+          <CheckList
+            @check="ChangeState($event)"
+            v-if="doingTodo.length > 0"
+            :todos="doingTodo"
+          />
+        </v-list>
+
+        <v-divider></v-divider>
+        <v-list subheader>
+          <v-subheader class="green--text">Done</v-subheader>
+          <CheckList
+            @check="ChangeState($event)"
+            v-if="doneTodo.length > 0"
+            :todos="doneTodo"
+          />
+        </v-list>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import { filter } from "vue/types/umd";
+import CheckList from "../components/CheckList";
 
 export default {
   name: "HomeView",
+
   components: {
-    HelloWorld,
+    CheckList,
+  },
+  data: () => ({
+    isAdd: false,
+    newTodo: "",
+    todo: [
+      {
+        todoState: "todo",
+        title: "Create Website",
+      },
+      {
+        todoState: "doing",
+        title: "Create Build",
+      },
+      {
+        todoState: "done",
+        title: "Create confirm",
+      },
+    ],
+  }),
+  computed: {
+    doingTodo() {
+      return this.todo.filter((x) => x.todoState == "doing");
+    },
+    doneTodo() {
+      return this.todo.filter((x) => x.todoState == "done");
+    },
+    thingsTodo() {
+      return this.todo.filter((x) => x.todoState == "todo");
+    },
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.length > 0) {
+        // console.log("you");
+        this.todo.unshift({ todoState: "todo", title: this.newTodo });
+        this.newTodo = "";
+      } else {
+        // this.$refs.title.validate();
+        console.log("me");
+      }
+    },
+    ChangeState(state) {
+      console.log(state.todoState);
+    },
   },
 };
 </script>
